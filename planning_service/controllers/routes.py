5b16ed31c7770da12e04af1fa6ffebcee02fe4da
romberol <bernikov.roman.43@gmail.com>
2024-05-12 17:26:42 +0300

    mongodb communication

--- planning_service/controllers/routes.py
+++ planning_service/controllers/routes.py
@@ -7,14 +7,66 @@ class PlanningService(Resource):
 
     def __init__(self):
         self.post_parser = reqparse.RequestParser()
-        self.post_parser.add_argument()
+        self.post_parser.add_argument('user_token', required=True, help="User token is required")
+        self.post_parser.add_argument('group_token', required=True, help="Group name is required")
+        self.post_parser.add_argument('description', required=True, help="Description is required")
+        self.post_parser.add_argument('users_list', required=True, help="List of users", type=list)
+        self.post_parser.add_argument('group_name', required=True, help="Group name is required")
+        self.post_parser.add_argument('event_time', required=True, help="Event time is required")
 
-        self.get_parser = reqparse.RequestParser()
+        self.delete_parser = reqparse.RequestParser()
+        self.delete_parser.add_argument('user_token', required=True, help="User token is required")
+        self.delete_parser.add_argument('event_token', required=True, help="Event token is required")
+
+        self.view_events_user_name_parser = reqparse.RequestParser()
+        self.view_events_user_name_parser.add_argument('user_name', required=True, help="User token is required")
+
+        self.view_events_group_parser = reqparse.RequestParser()
+        self.view_events_group_parser.add_argument('group_token', required=True, help="Group token is required")
+
+        self.view_events_creator_parser = reqparse.RequestParser()
+        self.view_events_creator_parser.add_argument('user_token', required=True, help="User token is required")
 
 
-    def post(self):
+    def post(self, operation=None):
+        if operation == "create_event":
+            args = self.post_parser.parse_args()
+            user_token = args['user_token']
+            group_token = args['group_token']
+            description = args['description']
+            users_list = args['users_list']
+            group_name = args['group_name']
+            event_time = args['event_time']
+
+            event_token = PostService.create_event(user_token, group_token, description,
+                                                   users_list, group_name, event_time)
+            return {'event_token': event_token}, 200
+        return None
+
+    def delete(self, operation=None):
+        if operation == "cancel_event":
+            args = self.delete_parser.parse_args()
+            user_token = args['user_token']
+            event_token = args['event_token']
+            PostService.cancel_event(user_token, event_token)
+            return None, 200
         return None
 
 
-    def get(self):
+    def get(self, operation=None):
+        if operation == "view_events_user_name":
+            args = self.view_events_user_name_parser.parse_args()
+            user_name = args['user_name']
+            events = GetService.view_events_user_name(user_name)
+            return {'events': events}, 200
+        elif operation == "view_events_group":
+            args = self.view_events_group_parser.parse_args()
+            group_token = args['group_token']
+            events = GetService.view_events_group(group_token)
+            return {'events': events}, 200
+        elif operation == "view_events_creator":
+            args = self.view_events_creator_parser.parse_args()
+            user_token = args['user_token']
+            events = GetService.view_events_creator(user_token)
+            return {'events': events}, 200
         return None
--- planning_service/repository/dbManipulation.py
+++ planning_service/repository/dbManipulation.py
@@ -1,25 +1,61 @@
-import json
+import pymongo
+from bson.objectid import ObjectId
+
+# MongoDB connection details
+MONGO_URI = "mongodb://localhost:27017"
+DATABASE_NAME = "test"
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]
+
+events_collection = db["events"]
+users_collection = db["users"]
 
 
 class communicateWithDB:
+    @staticmethod
+    def create_event(creator_token, group_token, event_description, users_list, group_name, event_time):
+        event_data = {
+            "creator_token": creator_token,
+            "group_token": group_token,
+            "description": event_description,
+            "users_list": [{"user_name": user.user_name, "user_email": user.user_email} for user in users_list],
+            "group_name": group_name,
+            "event_time": event_time
+        }
+        inserted_event = events_collection.insert_one(event_data)
+        return str(inserted_event.inserted_id)
 
     @staticmethod
-    def create_event(event_description, user_list, group_token):
-        event_id = 0
-        return event_id
+    def cancel_event(user_token, event_token):
+        event = events_collection.find_one({"_id": ObjectId(event_token)})
+        if event and event["creator_token"] == user_token:
+            group_name = event["group_name"]
+            events_collection.delete_one({"_id": ObjectId(event_token)})
+            return True, group_name
+        return False, None
 
     @staticmethod
-    def cancel_event(event_id):
-        return None
+    def view_user_events(user_name):
+        events = events_collection.find({"users_list.user_name": user_name})
+        event_tuples = [(str(event["_id"]), event["description"]) for event in events]
+        return event_tuples
 
     @staticmethod
-    def get_user_list(event_id):
-        return None
+    def view_events_group(group_token):
+        events = events_collection.find({"group_token": group_token})
+        event_tuples = [(str(event["_id"]), event["description"]) for event in events]
+        return event_tuples
 
     @staticmethod
-    def view_user_events(user_token):
-        return None
+    def view_events_creator(user_token):
+        events = events_collection.find({"creator_token": user_token})
+        event_tuples = [(str(event["_id"]), event["description"]) for event in events]
+        return event_tuples
 
     @staticmethod
-    def view_event_info(event_id):
-        return None
+    def get_user_list(event_token):
+        event = events_collection.find_one({"_id": ObjectId(event_token)})
+        if event:
+            return event["users_list"]
+        return []
